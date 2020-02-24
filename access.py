550b92fb32c6df743749a3dd3239a0321b18dc7a
MaxBi2019 <54325661+MaxBi2019@users.noreply.github.com>
2020-02-25 01:06:23 +0200

    Update access.py

--- access.py
+++ access.py
@@ -1,5 +1,5 @@
 def oauth():
-    return{"token_key":  "2761234941-2LKzik4eeonlkRc7VSm2ONKIJevmgu8pXd2FpAb",\
-           "token_secret": "JJLred45bxG7Xdp5njLQpHRmDRR58s2QGFU115ilPWSI8",\
-           "consumer_key":  "4J8trwf96XhJwcf5jZIzDfSbE",\
-           "consumer_secret": "rBypple4u9ZghE05BSWJaVIWRzEEV6wcsslVvQqvmVL8CIuOvA"}
+    return{"token_key":  "hidden",\
+           "token_secret": "hidden",\
+           "consumer_key":  "hidden",\
+           "consumer_secret": "hidden"}
